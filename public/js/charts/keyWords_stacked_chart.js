75bd9b5555d2ccae711761f6a6684115288c654d
JaimeLG <themightyjme@gmail.com>
2018-04-24 16:57:54 -0700

    Add files via upload

---
+++ public/js/charts/keyWords_stacked_chart.js
@@ -0,0 +1,42 @@
+Highcharts.chart('keyWords_stacked_chart', {
+    chart: {
+        type: 'column'
+    },
+    title: {
+        text: ''
+    },
+    xAxis: {
+        categories: Words["palabras_clave"]
+    },
+    yAxis: {
+        min: 0,
+        title: {
+            text: 'Número total'
+        }
+    },
+    tooltip: {
+        pointFormat: '<span style="color:{series.color}">{series.name}</span>: <b>{point.y}</b> ({point.percentage:.0f}%)<br/>',
+        shared: true
+    },
+    plotOptions: {
+        column: {
+            stacking: 'porcentaje'
+        }
+    },
+    series: getData()
+});
+
+function getData() {
+
+
+    data = [];
+
+    for (var candidato in Words["candidatos_palabras"]) {
+        data.push({
+            "name": candidato,
+            "data": Words["candidatos_palabras"][candidato]
+        });
+    }
+
+    return data;
+}
---
+++ public/js/charts/tweets_line_total.js
@@ -0,0 +1,66 @@
+Highcharts.chart('tweets_line_chart', {
+    chart: {
+        type: 'spline'
+    },
+    title: {
+        text: ' '
+    },
+    subtitle: {
+        text: 'candidatos'
+    },
+    /*xAxis: {
+        type: 'datetime',
+        labels: {
+            overflow: 'justify'
+        }
+        },*/
+    xAxis: {
+        type: 'datetime',
+        dateTimeLabelFormats: { // don't display the dummy year
+            month: '%e. %b',
+            year: '%b'
+        }
+    },
+    
+    plotOptions: {
+        spline: {
+            lineWidth: 4,
+            states: {
+                hover: {
+                    lineWidth: 5
+                }
+            },
+            marker: {
+                enabled: false
+            },
+            //pointInterval: 3600000, // one hour
+            //pointStart: Date.UTC(2018, 4, 20, 0, 0, 0)
+        }
+    },
+    yAxis: {
+        title: {
+            text: 'Tweets'
+        },
+       
+    },
+    tooltip: {
+        crosshairs: true,
+        shared: true
+    },
+    
+    series: validateData()
+});
+
+function validateData() {
+    candidates = [];
+    
+    for (var candidate in DatosSemanales) {
+        candidates.push({
+            name: candidate,
+            data: DatosSemanales[candidate]["tweets"]
+        });
+    }
+
+    return candidates;
+    
+}
